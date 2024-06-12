21f0e7a0eeba30af37d1fc82d5ab903e911b44aa
xBlackEaglex <roberto.smusic.1@gmail.com>
2024-06-12 13:23:54 -0600

    se inicio el curso

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { QrCodeModule } from 'ng-qrcode';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+	imports: [
+		CommonModule,
+		FormsModule,
+		IonicModule,
+		HomePageRoutingModule,
+		QrCodeModule,
+	],
+	declarations: [HomePage],
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,69 @@
 import { Component } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Share } from '@capacitor/share';
+import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+	segment = 'generate';
+	qrText = '';
 
-  constructor() {}
+	constructor(
+		private loadingController: LoadingController,
+		private platform: Platform
+	) {}
 
+	// === Captura el elemento html, convierte a canvas y obtiene una imagen ===
+	captureScreen() {
+		const element = document.getElementById('qrImage') as HTMLElement;
+		html2canvas(element).then((canvas: HTMLCanvasElement) => {
+
+			if (this.platform.is('capacitor')) this.ShareImage(canvas)
+			else this.downloadImage(canvas);
+
+		});
+	}
+
+	// === Descarga la imagen (web) ===
+	downloadImage(canvas: HTMLCanvasElement) {
+		const link = document.createElement('a');
+		link.href = canvas.toDataURL();
+		link.download = 'qr.png';
+		link.click();
+	}
+
+	// === Compartir la imagen (mobile) ===
+	async ShareImage(canvas: HTMLCanvasElement) {
+		let base64 = canvas.toDataURL();
+		let path = 'qr.png';
+
+		const loading = await this.loadingController.create({
+			spinner: 'crescent',
+		});
+		await loading.present();
+
+		await Filesystem.writeFile({
+			path: path,
+			data: base64,
+			directory: Directory.Cache,
+		}).then(async (res) => {
+				let uri = res.uri;
+
+				await Share.share({
+					url: uri,
+				});
+
+				await Filesystem.deleteFile({
+					path,
+					directory: Directory.Cache,
+				});
+			}).finally(() => {
+				loading.dismiss();
+			});
+	}
 }
